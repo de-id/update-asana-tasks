01a761330323429d3aa14a356df2338513e1996a
omer Tzadiki <omertzadiki@omertzadiki-MacBook-Pro.local>
2024-12-15 12:52:19 +0200

    x

--- src/release-notes.ts
+++ src/release-notes.ts
@@ -29,14 +29,23 @@ const getReleaseNotesFromDescriptions = (
     descriptionAndPrNumberArray.map(async ({ description }) => {
         try {
             const { taskUrls: taskUrlsFromCurrentDescription } =
-                await getTaskIdsAndUrlsFromPr(description);
+                getTaskIdsAndUrlsFromPr(description);
+            console.log(
+                'taskUrlsFromCurrentDescription',
+                taskUrlsFromCurrentDescription,
+                getTaskIdsAndUrlsFromPr(description)
+            );
             taskUrlsFromAllDescriptions = [
                 ...taskUrlsFromAllDescriptions,
                 ...taskUrlsFromCurrentDescription,
             ];
         } catch (e) {}
     });
-
+    console.log(
+        'taskUrlsFromAllDescriptions',
+        taskUrlsFromAllDescriptions,
+        descriptionAndPrNumberArray
+    );
     return `New release is being cooked 👩‍🍳, those are the asana tickets: 
     ${taskUrlsFromAllDescriptions.join(', ')}`;
 };
